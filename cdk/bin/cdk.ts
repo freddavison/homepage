682fc813e4fc4bbd4d22756c47795b86a4da0163
crochetcode <fred.davison@outlook.com>
2023-02-04 12:26:43 -0600

    :rocket: deploy s3 bucket

---
+++ cdk/bin/cdk.ts
@@ -0,0 +1,10 @@
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { CdkStack } from '../lib/cdk-stack';
+
+const app = new cdk.App();
+new CdkStack(app, 'CdkStack', {
+    env: {
+        region: 'us-east-1'
+    }
+});
