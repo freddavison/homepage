b78c0d53f0d1c2426685515fb86044ff386667f2
crochetcode <fred.davison@outlook.com>
2023-02-07 03:23:12 -0600

    switch to mobile-first approach

--- webpage/src/app.js
+++ webpage/src/app.js
@@ -1,12 +1,10 @@
 import styles from './styles/app.module.css';
+import {Logo} from './components/logo';
 
 function App() {
     return (
         <div className={`${styles.app}`}>
-            <div>
-                <h1>Fred</h1>
-                <h1>Davison</h1>
-            </div>
+          <Logo/>
         </div>
     );
 }
---
+++ webpage/src/components/logo.js
@@ -0,0 +1,10 @@
+import styles from '../styles/logo.module.css';
+
+export const Logo = () => {
+    return (
+        <div className={`${styles.logo}`}>
+            <h1>Fred</h1>
+            <h1>Davison</h1>
+        </div>
+    );
+}
