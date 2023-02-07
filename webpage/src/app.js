3615119913e8ae0735472124e0cb8413ae440903
crochetcode <fred.davison@outlook.com>
2023-02-07 04:35:19 -0600

    add header footer links

--- webpage/src/app.js
+++ webpage/src/app.js
@@ -1,10 +1,14 @@
 import styles from './styles/app.module.css';
+import {Header} from './components/header';
 import {Logo} from './components/logo';
+import {Footer} from './components/footer';
 
 function App() {
     return (
         <div className={`${styles.app}`}>
-          <Logo/>
+            <Header/>
+            <Logo/>
+            <Footer/>
         </div>
     );
 }
