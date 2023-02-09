e07a4693bbd0ed6626df962eed73f0b6b1336bed
crochetcode <fred.davison@outlook.com>
2023-02-08 19:19:36 -0600

    clean up

--- webpage/src/app.js
+++ webpage/src/app.js
@@ -1,5 +1,4 @@
 import styles from './styles/app.module.css';
-import {Header} from './components/header';
 import {Logo} from './components/logo';
 import {NotFound} from './components/not-found';
 import {Footer} from './components/footer';
@@ -8,7 +7,6 @@ import {Routes, Route} from 'react-router-dom';
 function App() {
     return (
         <div className={`${styles.app}`}>
-            <Header/>
             <Routes>
                 <Route path="/" element={<Logo/>}/>
                 <Route path="*" element={<NotFound/>}/>
