45f39d4c54012cd1b1dd382d787ff9eb182fd2aa
crochetcode <fred.davison@outlook.com>
2023-03-04 09:33:51 -0600

    add hover

--- webpage/src/components/square.js
+++ webpage/src/components/square.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 export const Square = props => {
     return (
         <Link to={props.link}>
-            <div className={'square flex items-center bg-georgia'}>
+            <div className={'square flex items-center border-4 hover:bg-georgia border-style-solid border-georgia'}>
                 {props.content}
             </div>
         </Link>
