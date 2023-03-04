082f768c42ff75ed85c4e49dd5a3ccad71463e74
crochetcode <fred.davison@outlook.com>
2023-03-04 09:22:21 -0600

    remove color toggles

--- webpage/src/app.js
+++ webpage/src/app.js
@@ -1,32 +1,20 @@
 import './styles/input.css';
 import {Routes, Route} from 'react-router-dom';
-import {useState} from 'react';
-import {Header} from './components/header';
 import {Footer} from './components/footer';
 import {Square} from './components/square';
 import {about, landing, notFound} from './constants/content';
 
 const App = () => {
-    const [index, changeIndex] = useState(0);
-    const changeColor = () => {
-        if (index === 2){
-             changeIndex(0);
-        } else {
-            changeIndex(index + 1);
-        }
-    };
-
     return (
         <>
             <div className='main flex items-center justify-center items-center'>
-                <Header index={index} changeColor={changeColor}/>
                 <Routes>
-                    <Route path='/' element={<Square link='/about' content={landing} index={index}/>}/>
-                    <Route path='/about' element={<Square link='/' content={about} index={index}/>}/>
-                    <Route path='/*' element={<Square link='/' content={notFound} index={index}/>}/>
+                    <Route path='/' element={<Square link='/about' content={landing}/>}/>
+                    <Route path='/about' element={<Square link='/' content={about}/>}/>
+                    <Route path='/*' element={<Square link='/' content={notFound}/>}/>
                 </Routes>
             </div>
-            <Footer index={index}/>
+            <Footer/>
         </>
     );
 }
--- webpage/src/components/square.js
+++ webpage/src/components/square.js
@@ -1,19 +1,9 @@
 import {Link} from 'react-router-dom';
 
-const GEORGIA = 'square flex items-center bg-georgia';
-const FIDELITY = 'square flex items-center bg-fidelity';
-const LIBERTY = 'square flex items-center bg-liberty-200 text-liberty-100';
-
-const getCss = index => {
-    if (index === 0) return GEORGIA;
-    if (index === 1) return FIDELITY;
-    return LIBERTY;
-};
-
 export const Square = props => {
     return (
         <Link to={props.link}>
-            <div className={getCss(props.index)}>
+            <div className={'square flex items-center bg-georgia'}>
                 {props.content}
             </div>
         </Link>
