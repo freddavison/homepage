c8dbd1d78e7b7ed36674f754960253615e0dd99d
crochetcode <fred.davison@outlook.com>
2023-03-08 18:33:15 -0600

    add window

--- webpage/components/card.js
+++ webpage/components/card.js
@@ -4,18 +4,20 @@ import {Button} from './button';
 export const Card = () => {
     return (
         <>
-            <div className='card bg-primary py-10 shadow-xl flex items-center p-3 mb-5'>
-                <h1 className='text-5xl font-bold text-base-100'>Fred Davison</h1>
-                <h2 className='text-3xl text-base-100'>homepage and links</h2>
-                <div className='avatar m-5'>
-                    <div className='w-36 mask mask-squircle'>
-                        <img
-                            src='https://avatars.githubusercontent.com/u/76531816?s=400&u=3d116c97bc79963f5e3855a89c57b004332e28a3&v=4'/>
+            <div className='mockup-window shadow-xl bg-base-300'>
+                <div className='flex justify-center py-5 px-10 md:px-20 bg-primary flex items-center flex-col'>
+                    <h1 className='text-3xl md:text-5xl font-bold text-base-100'>Fred Davison</h1>
+                    <h2 className='text-xl md:text-3xl text-base-100'>homepage and links</h2>
+                    <div className='avatar m-5'>
+                        <div className='w-32 md:w-40 mask mask-squircle'>
+                            <img
+                                src='https://avatars.githubusercontent.com/u/76531816?s=400&u=3d116c97bc79963f5e3855a89c57b004332e28a3&v=4'/>
+                        </div>
                     </div>
+                    <Button link={'https://github.com/freddavison'} icon={faGithub}/>
+                    <Button link={'https://www.linkedin.com/in/fdavison1'} icon={faLinkedin}/>
+                    <Button link={'https://www.instagram.com/midwest_fred'} icon={faInstagram}/>
                 </div>
-                <Button link={'https://github.com/freddavison'} icon={faGithub}/>
-                <Button link={'https://www.linkedin.com/in/fdavison1'} icon={faLinkedin}/>
-                <Button link={'https://www.instagram.com/midwest_fred'} icon={faInstagram}/>
             </div>
         </>
     );
