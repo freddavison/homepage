dcefd5a05866642c37c68b402f9fb0fc713372af
crochetcode <fred.davison@outlook.com>
2023-03-09 19:41:56 -0600

    align labels

--- webpage/components/button.js
+++ webpage/components/button.js
@@ -5,7 +5,7 @@ export const Button = props => {
         <a href={props.link} rel='noreferrer' target={'_blank'}>
             <button className={'btn btn-accent md:btn-lg m-1 px-10 md:w-60 md:px-5 flex justify-start'}>
                 <FontAwesomeIcon icon={props.icon} className={'text-4xl md:text-5xl px-4'}/>
-                <p className={'hidden md:inline m-auto'}>{props.label}</p>
+                <p className={'hidden md:inline'}>{props.label}</p>
             </button>
         </a>
     );
