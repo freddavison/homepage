c5135923afe7e6c2f92962080f648d161f54de4b
crochetcode <fred.davison@outlook.com>
2023-07-17 19:03:35 -0400

    add instagram link

--- webpage/components/card.js
+++ webpage/components/card.js
@@ -1,23 +1,22 @@
-import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import {faGithub, faInstagram, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 import {Button} from './button';
 
-export const Card = () => {
-    return (
-        <>
-            <div className='mockup-window shadow-xl bg-base-300'>
-                <div className='flex justify-center py-5 px-10 md:px-20 bg-primary flex items-center flex-col'>
-                    <h1 className='text-4xl md:text-5xl font-bold text-base-100'>Fred Davison</h1>
-                    <h2 className='text-2xl md:text-3xl text-base-100'>homepage and links</h2>
-                    <div className='avatar m-5'>
-                        <div className='w-40 mask mask-squircle'>
-                            <img
-                                src='https://avatars.githubusercontent.com/u/76531816?s=400&u=3d116c97bc79963f5e3855a89c57b004332e28a3&v=4'/>
-                        </div>
+export const Card = () => (
+    <>
+        <div className='mockup-window shadow-xl bg-base-300'>
+            <div className='flex justify-center py-5 px-10 md:px-15 bg-primary flex items-center flex-col'>
+                <h1 className='text-4xl md:text-5xl font-bold text-base-100'>Fred Davison</h1>
+                <h2 className='text-2xl md:text-3xl text-base-100'>homepage and links</h2>
+                <div className='avatar m-5'>
+                    <div className='w-36 mask mask-squircle'>
+                        <img
+                            src='https://avatars.githubusercontent.com/u/76531816?s=400&u=3d116c97bc79963f5e3855a89c57b004332e28a3&v=4'/>
                     </div>
-                    <Button link={'https://github.com/freddavison'} icon={faGithub} label={'GitHub'}/>
-                    <Button link={'https://www.linkedin.com/in/fdavison1'} icon={faLinkedin} label={'LinkedIn'}/>
                 </div>
+                <Button link={'https://github.com/freddavison'} icon={faGithub} label={'GitHub'}/>
+                <Button link={'https://www.linkedin.com/in/fdavison1'} icon={faLinkedin} label={'LinkedIn'}/>
+                <Button link={'https://www.instagram.com/hilltown.fred'} icon={faInstagram} label={'Instagram'}/>
             </div>
-        </>
-    );
-};
+        </div>
+    </>
+);
