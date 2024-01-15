c5d0a32e29a39161a3464fdea5f80e4daa23205f
crochetcode <fred.davison@outlook.com>
2024-01-15 14:15:03 -0500

    :wrench: update nextConfig

--- webpage/next.config.js
+++ webpage/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    output: 'export',
+    trailingSlash: true
+}
 
 module.exports = nextConfig
